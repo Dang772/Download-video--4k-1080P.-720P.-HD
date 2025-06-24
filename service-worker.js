0878e44376210e0cb8e3bc6eafa6a874d2b706d9
Dang <mydangg1999@gmail.com>
2025-06-24 17:31:35 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 self.addEventListener('install', event => {
-    console.log('Service Worker installed');
-    self.skipWaiting();
+  console.log('Service Worker installing.');
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-    console.log('Service Worker activated');
+  console.log('Service Worker activated.');
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request));
 });
